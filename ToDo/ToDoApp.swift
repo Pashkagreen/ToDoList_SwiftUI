d9becfc87416217aea03cb392c5b13c5c968aac1
Paul Grin <pashkagrin@gmail.com>
2024-03-12 00:08:57 +0100

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Paul Grin on 12/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

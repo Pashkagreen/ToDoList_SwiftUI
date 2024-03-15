e25af82396065c8679071deecb7d96f90d220a23
Paul Grin <pashkagrin@gmail.com>
2024-03-15 23:25:31 +0100

    feat: init

--- ToDo/Other/ToDoApp.swift
+++ ToDo/Other/ToDoApp.swift
@@ -4,11 +4,15 @@
 //
 //  Created by Paul Grin on 12/03/2024.
 //
-
+import FirebaseCore
 import SwiftUI
 
 @main
 struct ToDoApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()

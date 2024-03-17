b7ac3b5c9c693e92bc91537b7aad64345af0916a
Paul Grin <pashkagrin@gmail.com>
2024-03-17 23:32:50 +0100

    feat: finish

--- ToDo/ViewModels/ToDoListItemViewViewModel.swift
+++ ToDo/ViewModels/ToDoListItemViewViewModel.swift
@@ -4,7 +4,8 @@
 //
 //  Created by Paul Grin on 15/03/2024.
 //
-
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 /// View model for a single todo list item view
@@ -12,6 +13,12 @@ class ToDoListItemViewViewModel: ObservableObject {
     init() {}
 
     func toggleIsDone(item: ToDoListItem) {
-        
+        var newItem = item
+        newItem.setDone(!item.isDone)
+
+        guard let userId = Auth.auth().currentUser?.uid else { return }
+
+        let db = Firestore.firestore()
+        db.collection("users").document(userId).collection("todos").document(newItem.id).updateData(newItem.asDictionary())
     }
 }
--- ToDo/Views/ProfileView.swift
+++ ToDo/Views/ProfileView.swift
@@ -12,9 +12,59 @@ struct ProfileView: View {
 
     var body: some View {
         NavigationView {
-            VStack {}
-                .navigationTitle("Profile")
+            VStack {
+                if let user = viewModel.user {
+                    userProfile(user: user)
+                } else {
+                    Text("Loading...")
+                }
+            }
+            .navigationTitle("Profile")
         }
+        .onAppear {
+            viewModel.fetchUser()
+        }
+    }
+
+    @ViewBuilder
+    func userProfile(user: User) -> some View {
+        // Avatar
+        Image(systemName: "person.circle")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .foregroundColor(.blue)
+            .frame(width: 125, height: 125)
+            .padding()
+
+        // Info
+        VStack(alignment: .leading) {
+            HStack {
+                Text("Name: ")
+                    .bold()
+                Text(viewModel.user?.name ?? "")
+            }
+
+            HStack {
+                Text("Email: ")
+                    .bold()
+                Text(viewModel.user?.email ?? "")
+            }
+
+            HStack {
+                Text("Member since: ")
+                    .bold()
+                Text("\(Date(timeIntervalSince1970: viewModel.user?.joinedAt ?? 0).formatted(date: .abbreviated, time: .shortened))")
+            }
+        }
+
+        // Sign Out
+        Button("Sign Out") {
+            viewModel.signOut()
+        }
+        .tint(.red)
+        .padding()
+
+        Spacer()
     }
 }
 
